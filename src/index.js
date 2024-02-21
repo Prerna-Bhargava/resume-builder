e6e94be530d6f1c779d9fb81ee620563d200c58d
Prerna-Bhargava <prerna172002@gmail.com>
2024-02-21 23:55:21 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -7,15 +7,9 @@ import { ResumeProvider } from './context/ResumeProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <ResumeProvider>
       <App />
     </ResumeProvider>
 
-  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
